a2f1148a3ade06081c18fa83758c0b13f0faaf9b
czetoj <czeto.jozsef.0229@gmail.com>
2021-09-06 15:03:18 +0200

    4.feladat

--- backend/src/controllers/building/building.service.js
+++ backend/src/controllers/building/building.service.js
@@ -7,6 +7,8 @@
  * A @getAll metódus adja vissza a populált teljes "building" listát
  */
 
-exports.update = (buildingId, className) => {};
+const Model = require('../../models/building.model');
 
-exports.getAll = () => {}
+exports.update = (buildingId, className) => Model.findByIdAndUpdate(buildingId, className, { new: true });
+
+exports.getAll = () => Model.find().populate('classrooms');
